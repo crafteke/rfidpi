1b4ca12fe4332cf13b165becdcf7216a0344e8c6
rfidpi <lapins@crafteke.be>
2022-04-23 18:45:12 +0100

    added calibration

--- controller.py
+++ controller.py
@@ -14,11 +14,32 @@ serials=[None]*6
 
 correct_tags=["0415911acdc826","0415917a9b5728","0415910a76d926","0415910a66c326","0415918a11ac28","0415911a2ce826"]
 GPIO.setmode(GPIO.BCM)
+VALIDATE_BUTTON=4
+START_BUILDING=17
 #
 # @sio.event
 # def connect():
 #     print("Connected to server. Registering.")
 #     sio.emit('Register','rfidpi')
+
+def push_validate(channel):
+    msg={}
+    print("Validation button pushed.")
+    for i in range(0,6):
+        if(serials[i]!= None):
+            serials[i].write(bytes('1', 'utf-8'))
+    msg["controller_id"]="rfid_button_validate"
+    msg['value']=1
+    if sio.connected:
+        sio.emit('Command',msg)
+
+def push_build(channel):
+    msg={}
+    print("Launch button pushed.")
+    msg["controller_id"]="rfid_button_launch"
+    msg['value']=1
+    if sio.connected:
+        sio.emit('Command',msg)
 
 @sio.event
 def connect_error():
@@ -58,6 +79,11 @@ def main():
                 except:
                     print("Error connecting ", ser_port," Retry...")
             retries=0
+
+    GPIO.setup(VALIDATE_BUTTON, GPIO.IN,pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(START_BUILDING,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+    GPIO.add_event_detect(VALIDATE_BUTTON, GPIO.FALLING, callback=push_validate, bouncetime=200)
+    GPIO.add_event_detect(START_BUILDING, GPIO.FALLING, callback=push_build,bouncetime=200)
     while True:
         for i in range(0,6):
             if(serials[i]!= None and serials[i].in_waiting):
@@ -65,21 +91,29 @@ def main():
                 data=data.decode("utf-8").strip()
                 print("Received RFID:",data)
                 msg={}
-                rfid_id=data.split(':')[0]
+                serial_msg=data.split(':')
+                rfid_id=serial_msg[0]
                 if(rfid_id in ["0","1","2","3","4","5"]): #ignore firmware dumping from arduino
                     msg["controller_id"]= "rfid_"+rfid_id
-                    rfid=data.split(':')[1]
+                    rfid=serial_msg[1]
                     if(rfid=="TAG_GONE"):
-                        msg["value"]=-1
-                    elif rfid==correct_tags[i]:
-                        msg["value"]=1
-                    elif(rfid=="TAG_FOUND"):
                         msg["value"]=0
+                    elif rfid==correct_tags[i]:
+                        if(len(serial_msg)==3 and serial_msg[2]=='R'):
+                            msg["value"]=2
+                        else:
+                            msg["value"]=1
+                    else:
+                        if(len(serial_msg)==3 and serial_msg[2]=='R'):
+                            msg["value"]=-2
+                        else:
+                            msg["value"]=-1
                     if sio.connected:
                         sio.emit('Command',msg)
         time.sleep(0.1)
 
-
+def requestRefresh():
+    serials.write(1)
 def handler(signal_received, frame):
     # Handle any cleanup here
     print('SIGINT or CTRL-C detected. Exiting gracefully')
@@ -88,7 +122,15 @@ def handler(signal_received, frame):
         if(ser != None):
             ser.close()
     #GPIO.cleanup()
+
     exit(0)
+@sio.event
+def Command(data):
+    if(data['controller_id']=='rfid_button_calibrate' and data['value']=='1'):
+        print("Calibration of rfid.")
+        for i in range(0,6):
+            if(serials[i]!= None):
+                serials[i].write(bytes('2', 'utf-8'))
 
 if __name__ == "__main__":
     signal(SIGINT, handler)
--- duinoRFID/duinoRFID.ino
+++ duinoRFID/duinoRFID.ino
@@ -1,12 +1,15 @@
 #include <SPI.h>
 #include <MFRC522.h>
-
+#include <EEPROM.h>
 
 #define RST_PIN         9          // Configurable, see typical pin layout above
 #define SS_PIN          3          // Configurable, see typical pin layout above
 
 
-#define READER_ID       04242
+//#define READER_ID       04242
+//#define READER_ID       0
+int READER_ID=0;
+String correct_tags[]={"0415911acdc826","0415917a9b5728","0415910a76d926","0415910a66c326","0415918a11ac28","0415911a2ce826"};
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
 
@@ -14,18 +17,42 @@ bool rfid_tag_present_prev = false;
 bool rfid_tag_present = false;
 int _rfid_error_counter = 0;
 bool _tag_found = false;
-
+bool refresh_request=false;
+bool eeprom_id_calibrate=false;
 void setup() {
  Serial.begin(115200);		// Initialize serial communications with the PC
  while (!Serial);		// Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
  SPI.begin();			// Init SPI bus
  mfrc522.PCD_Init();		// Init MFRC522
  mfrc522.PCD_DumpVersionToSerial();
+ READER_ID=EEPROM.read(0);
 
 }
 
 //byte tag_read[]
 void loop() {
+  while (Serial.available() > 0) {
+     // read the incoming byte:
+     int d=Serial.readString().toInt();
+       //   Serial.print("DEBUG:");
+       // Serial.print(READER_ID);
+       // Serial.print(": refresh request. DATA=");
+       // Serial.println(d, DEC); // here, DEC means Decimal
+     if(d==1){
+       rfid_tag_present_prev = false;
+       rfid_tag_present = false;
+       _rfid_error_counter = 0;
+       _tag_found = false;
+       refresh_request=true;
+     }
+     if(d==2){
+       rfid_tag_present_prev = false;
+       rfid_tag_present = false;
+       _rfid_error_counter = 0;
+       _tag_found = false;
+       eeprom_id_calibrate=true;
+     }
+   }
  rfid_tag_present_prev = rfid_tag_present;
 
  _rfid_error_counter += 1;
@@ -60,11 +87,17 @@ void loop() {
  if (rfid_tag_present && !rfid_tag_present_prev){
    //sendData("TAG_FOUND"); useless: just send tag and we know there was something
    sendTag();
+   if(eeprom_id_calibrate){
+     calibrateEEPROM();
+   }
  }
 
  // falling edge
  if (!rfid_tag_present && rfid_tag_present_prev){
     sendData("TAG_GONE");
+ }
+ if(refresh_request){ //refresh was asked but no rfid on reader
+   refresh_request=false;
  }
 }
 
@@ -91,5 +124,25 @@ void sendTagSpaced(){
 
 void sendData(String s){
   Serial.print(READER_ID);
+  if(refresh_request){
+    s=s+':'+'R';
+    refresh_request=false;
+  }
   Serial.println(":"+s);
+}
+void calibrateEEPROM(){
+  String tag="";
+  for(int i=0;i<mfrc522.uid.size;i++){
+    tag.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? "0" : ""));
+    tag.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  for(int i=0;i<6;i++){
+    if(tag==correct_tags[i]){
+      EEPROM.update(0, i);
+    }
+  }
+  READER_ID=EEPROM.read(0);
+  Serial.print("CALIBRATED:");
+  Serial.println(READER_ID);
+  eeprom_id_calibrate=false;
 }
